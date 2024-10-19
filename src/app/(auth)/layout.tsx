9302b7ef22fb49f58023141ca6ba66178c5ef0b1
Krisno Mukti <typescript97@gmail.com>
2024-10-19 03:47:01 +0000

    fix(auth): fixed several errors in the authentication layout component

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -9,13 +9,13 @@ export default function AuthLayout({
   return (
     <main className='bg-neutral-100 min-h-screen'>
       <div className='mx-auto max-w-screen-2xl p-5'>
-        <nav className='flex items-center justify-center'>
-          <Image src='/loto.svg' alt='Logo' width={152} height={56} />
+        <nav className='flex items-center justify-between'>
+          <Image src='/logo.svg' alt='Logo' width={152} height={56} />
           <Button variant='secondary' size='lg'>
             Sign up
           </Button>
         </nav>
-        <div className='flex items-center justify-center pt-4 lg:pt-14'>
+        <div className='flex items-center justify-center pt-4 md:pt-14'>
           {children}
         </div>
       </div>
---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,5 @@
+import SignUpCard from '@/features/components/sign-up-card';
+
+export default function SignUpPage() {
+  return <SignUpCard />;
+}
